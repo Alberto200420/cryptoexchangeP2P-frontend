4346819b64ecb310d89e31227c1c15e2f4ed4bf9
Meza A <albertog1meza@gmail.com>
2024-06-16 18:53:57 -0600

    nuevo intento

--- components/client/BicoinPrice.tsx
+++ components/client/BicoinPrice.tsx
@@ -10,12 +10,8 @@ export default function BitcoinPrice() {
   useEffect(() => {
     const fetchPrice = async () => {
       try {
-        const response  = await fetch('/api/btc-price', { method: 'GET' });
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        setBitcoinPrice(data.price);
+        let price = await calcularPrecioBTCenMXN();
+        setBitcoinPrice(price);
       } catch (error) {
         console.error('Error fetching BTC price:', error);
       }
